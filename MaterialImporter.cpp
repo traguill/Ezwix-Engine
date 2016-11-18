a9838b871f6cc5eb8e10731b4274e45226919a28
traguill <traguill1@gmail.com>
2016-11-18 18:00:50 +0100

    Import textures corrected

--- MaterialImporter.cpp
+++ MaterialImporter.cpp
@@ -12,27 +12,35 @@ bool MaterialImporter::Import(const char* file, const char * path)
 {
 	bool ret = false;
 
-	ILuint id;
-	ilGenImages(1, &id);
-	ilBindImage(id);
-	ilLoadImage(path);
-	
-
-	ILuint size;
-	ILubyte *data;
-	ilSetInteger(IL_DXTC_FORMAT, IL_DXT5);
-	size = ilSaveL(IL_DDS, NULL, 0);
+	char* buffer = nullptr;
+	unsigned int size = App->file_system->Load(path, &buffer);
 
 	if (size > 0)
 	{
-		data = new ILubyte[size];
-		if (ilSaveL(IL_DDS, data, size) > 0)
+		ILuint id;
+		ilGenImages(1, &id);
+		ilBindImage(id);
+
+		if (ilLoadL(IL_TYPE_UNKNOWN, (const void*)buffer, size))
 		{
-			ret = App->file_system->Save(file, data, size);
+			ILuint il_size;
+			ILubyte *data;
+			ilSetInteger(IL_DXTC_FORMAT, IL_DXT5);
+			il_size = ilSaveL(IL_DDS, NULL, 0);
+			if (il_size > 0)
+			{
+				data = new ILubyte[il_size];
+				if (ilSaveL(IL_DDS, data, il_size) > 0)
+				{
+					ret = App->file_system->Save(file, data, il_size);
+				}
+				delete[] data;
+				ilDeleteImages(1, &id);
+			}
 		}
-		delete[] data;
-		data = nullptr;
 	}
+
+	delete[] buffer;
 
 	return ret;
 }
